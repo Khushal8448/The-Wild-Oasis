3655fd4b2409a1a47494feedbfc50e3f990346d6
Khushal Mali <khushalmali8448@gmail.com>
2024-02-20 11:15:51 +0530

    adding reusable sort feature

---
+++ src/ui/SortBy.jsx
@@ -0,0 +1,12 @@
+import { useSearchParams } from "react-router-dom";
+import Select from "./Select";
+
+function SortBy({ options }) {
+  const [searchParams, setSearchParams] = useSearchParams();
+
+  function handleChange(e) {}
+
+  return <Select options={options} type="white" onChange={handleChange} />;
+}
+
+export default SortBy;
