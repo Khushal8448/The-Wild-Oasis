3e5c8aaa4fed1e04d747c564e9c7867ed81758d8
Khushal Mali <khushalmali8448@gmail.com>
2024-02-25 10:16:15 +0530

    adding API-side filtering feature for bookings

--- src/services/apiBookings.js
+++ src/services/apiBookings.js
@@ -1,19 +1,24 @@
 import { getToday } from "../utils/helpers";
 import supabase from "./supabase";
 
-export async function getBookings() {
-  let { data: bookings, error } = await supabase
+export async function getBookings({ filter, sortBy }) {
+  let query = supabase
     .from("bookings")
     .select(
       "id, created_at, startDate, endDate, numGuests, numNights, status, totalPrice, cabins(name), guests(fullName, email)"
     );
+
+  // Filter
+  if (filter !== null) query.eq(filter.field, filter.value);
+
+  let { data, error } = await query;
 
   if (error) {
     console.error(error);
     throw new Error("Bookings could not be loaded");
   }
 
-  return bookings;
+  return data;
 }
 
 export async function getBooking(id) {
