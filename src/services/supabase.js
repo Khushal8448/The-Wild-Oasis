62b713edad1bdbf42569960974004d3c4ff95145
Khushal Mali <khushalmali8448@gmail.com>
2024-05-29 22:01:14 +0530

    login hook

--- src/services/supabase.js
+++ src/services/supabase.js
@@ -1,8 +1,8 @@
 import { createClient } from "@supabase/supabase-js";
 
-export const supabaseUrl = "https://dclaevazetcjjkrzczpc.supabase.co";
+export const supabaseUrl = "https://lustegvvzmmcppzedesw.supabase.co";
 const supabaseKey =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRjbGFldmF6ZXRjamprcnpjenBjIiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODMyOTIzNDQsImV4cCI6MTk5ODg2ODM0NH0.LGg0M-taoHgKtxCzr9owrb09epnPaO_Yfz6xVE54sIY";
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imx1c3RlZ3Z2em1tY3BwemVkZXN3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDc0ODkyNTMsImV4cCI6MjAyMzA2NTI1M30.ZXugRm_ZiJerAQvuQ6RP5AdvqycL2qFED4bqD3lrA2I";
 const supabase = createClient(supabaseUrl, supabaseKey);
 
 export default supabase;
